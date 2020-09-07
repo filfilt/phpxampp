49c2ac1d1f2a641ec13baee7a862c436a03a4920
filfilt <tafneg4@gmail.com>
2020-09-07 15:24:45 -0400

    variable

---
+++ 003.variable code.php
@@ -0,0 +1,11 @@
+<?php
+$value=25;
+$name="Nega";
+$value=($value*8);
+echo "Hello  ".$name .' <br>';
+echo $name ."   ". $value .'<br>';
+echo "25x8 = ".$value.'<br/>';
+$google='Google Link';
+echo '<a href="http://www.Google.com">'.$google.'</a>';
+
+?>
